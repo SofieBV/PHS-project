9d993a83a34bc635074ddb0e4d24054fbf6d8fdc
Sofie Verhees <s.b.verhees@sms.ed.ac.uk>
2022-02-03 16:43:29 +0000

    plotted some datasets quarterly

--- final_code/data_formatting.py
+++ final_code/data_formatting.py
@@ -58,48 +58,70 @@ def day_to_month(dataframe):
 
     return dataframe
 
-def month_to_quarter(dataframe):
-    indices = dataframe.index.names
-    dataframe = dataframe.reset_index()
-    months = list(map(str,dataframe['Month']))
-    quarter = months.copy()
-    for i in range(len(months)):
-        if int(months[i][-1]) <= 3:
-            quarter[i] = months[i][:-2]+'Q1'
-        elif int(months[i][-1]) <= 6:
-            quarter[i] = months[i][:-2]+'Q2' 
-        elif int(months[i][-1]) <= 9:
-            quarter[i] = months[i][:-2]+'Q3'
-        else:
-            quarter[i] = months[i][:-2]+'Q4'  
-    dataframe['Month'] = quarter
-    dataframe = dataframe.set_index(indices).rename_axis(index={'Month':'Quarter'})
-    dataframe = dataframe.groupby(dataframe.index.names).sum()
-    return dataframe
+def month_to_quarter(dataframes, islist=False):
+    if not islist:
+        dataframes = [dataframes]
+    new_dataframes =[]
+    for dataframe in dataframes:
+        indices = dataframe.index.names
+        dataframe = dataframe.reset_index()
+        months = list(map(str,dataframe['Month']))
+        quarter = months.copy()
+        for i in range(len(months)):
+            if int(months[i][-2:]) <= 3:
+                quarter[i] = months[i][:-2]+'Q1'
+            elif int(months[i][-2:]) <= 6:
+                quarter[i] = months[i][:-2]+'Q2' 
+            elif int(months[i][-2:]) <= 9:
+                quarter[i] = months[i][:-2]+'Q3'
+            else:
+                quarter[i] = months[i][:-2]+'Q4'  
+        dataframe['Month'] = quarter
+        dataframe = dataframe.set_index(indices).rename_axis(index={'Month':'Quarter'})
+        dataframe = dataframe.groupby(dataframe.index.names).sum()
+        new_dataframes.append(dataframe)
+    if len(new_dataframes)==1:
+        return new_dataframes[0]
+    else:
+        return new_dataframes
 
 def day_to_quarter(dataframe):
     dataframe = day_to_month(dataframe)
     dataframe = month_to_quarter(dataframe)
     return dataframe
 
-def HB_to_areas(dataframe):
+def HB_to_areas(dataframes, islist=False):
+    if not islist:
+        dataframes = [dataframes]
+    new_dataframes =[]
+    for dataframe in dataframes:
+        dataframe = dataframe.rename(index={'S08000020':'NCA','S08000022':'NCA', 'S08000025':'NCA', 'S08000026':'NCA', 'S08000030':'NCA', \
+                                                'S08000028':'NCA', 'S08000016':'SCAN','S08000017':'SCAN','S08000029':'SCAN','S08000024':'SCAN', \
+                                                    'S08000015':'WOSCAN','S08000019':'WOSCAN','S08000031':'WOSCAN','S08000032':'WOSCAN'})
+        dataframe = dataframe.groupby(dataframe.index.names).sum()
+        new_dataframes.append(dataframe)
+        
+    if len(new_dataframes)==1:
+        return new_dataframes[0]
+    else:
+        return new_dataframes
 
-    dataframe = dataframe.rename(index={'S08000020':'NCA','S08000022':'NCA', 'S08000025':'NCA', 'S08000026':'NCA', 'S08000030':'NCA', \
-                                            'S08000028':'NCA', 'S08000016':'SCAN','S08000017':'SCAN','S08000029':'SCAN','S08000024':'SCAN', \
-                                                'S08000015':'WOSCAN','S08000019':'WOSCAN','S08000031':'WOSCAN','S08000032':'WOSCAN'})
+def time_interval(dataframes, interval, islist=False):
+    if not islist:
+        dataframes = [dataframes]
+    time_names = ['Month', 'Quarter', 'QuarterEnding', 'Date', 'Year']
+    new_dataframes = []
+
+    for dataframe in dataframes:
+        for ind in dataframe.index.names:
+            if ind in time_names:
+                newData = dataframe.loc[interval[0]:interval[1],:]
+        new_dataframes.append(newData)
     
-    dataframe = dataframe.groupby(dataframe.index.names).sum()
-
-    return dataframe
-
-def time_interval(dataframe, interval):
-    time_names = ['Month', 'MonthEnding', 'Quarter', 'QuarterEnding', 'Date', 'Year']
-
-    for ind in dataframe.index.names:
-        if ind in time_names:
-            newData = dataframe.loc[interval[0]:interval[1],:]
-
-    return newData
+    if len(new_dataframes)==1:
+        return new_dataframes[0]
+    else:
+        return new_dataframes
 
 def add_categories(dataframe, groupings):
     """
---
+++ final_code/plot_data.py
@@ -0,0 +1,66 @@
+from data_formatting import day_to_month, day_to_quarter, import_datasets, time_interval, add_categories, \
+                                HB_to_areas, extract_data, day_to_quarter, month_to_quarter
+import numpy as np
+import matplotlib.pyplot as plt
+
+data31, data62, operations, diag, covid, beds, emerg = import_datasets(['31DayData', '62DayData', 'cancellations_by_board_november_2021', \
+                                                         'diagnostics_by_board_september_2021', 'covid_2022', 'hospital_beds', \
+                                                             'monthly_ae_waitingtimes_202111'])
+covid = day_to_quarter(covid)
+operations, diag, emerg = month_to_quarter([operations, diag, emerg], islist=True)
+data31, data62, operations, diag, covid, beds, emerg = time_interval([data31, data62, operations, diag, covid, beds, emerg], \
+                                                                         ['2017Q1', '2021Q3'], islist=True)
+data31, data62, operations, diag, covid, beds, emerg = HB_to_areas([data31, data62, operations, diag, covid, beds, emerg],islist=True)
+
+for dataset in [data31, data62, operations, diag, covid, beds, emerg]:
+    print(dataset.index.names)
+    dataset.info()
+
+
+fig, ax = plt.subplots(3, 2, figsize=(16, 16), constrained_layout=True)
+ax = ax.ravel()
+
+for area in ['NCA', 'SCAN', 'WOSCAN']:
+    r31, t31 = extract_data(data31, (area, area,'All Cancer Types'), ['HB', 'HBT','CancerType'], \
+                                            ['NumberOfEligibleReferrals31DayStandard', 'NumberOfEligibleReferralsTreatedWithin31Days'])
+    r62, t62 = extract_data(data62, (area, area,'All Cancer Types'), ['HB', 'HBT','CancerType'], \
+                                            ['NumberOfEligibleReferrals62DayStandard', 'NumberOfEligibleReferralsTreatedWithin62Days'])
+    op_tot, op_can, op_cap = extract_data(operations, area, 'HBT', ['TotalOperations', 'TotalCancelled', 'NonClinicalCapacityReason'])
+    di4, di6 = extract_data(diag, (area, 'Imaging','All Imaging'), ['HBT', 'DiagnosticTestType','DiagnosticTestDescription'], \
+                                        ['NumberWaitingOverFourWeeks', 'NumberWaitingOverSixWeeks'])
+    de4, de6 = extract_data(diag, (area, 'Endoscopy','All Endoscopy'), ['HBT', 'DiagnosticTestType','DiagnosticTestDescription'], \
+                                        ['NumberWaitingOverFourWeeks', 'NumberWaitingOverSixWeeks'])
+    cov_pos, cov_death = extract_data(covid, area, 'HB', ['DailyPositive', 'DailyDeaths'])
+
+    add = np.concatenate(([r31[0,:-int(len(cov_pos[0,:]))]], [np.zeros(len(r31[0,:])-len(cov_pos[0,:]))]), axis=0)
+    cov_pos = np.concatenate((add, cov_pos), axis=1)
+    cov_death = np.concatenate((add, cov_death), axis=1)
+
+    ax[0].set_title('Cancer waiting times 31 day standard')
+    ax[0].plot(r31[0,:],r31[1,:], label='31 day referrals in {}'.format(area))
+    ax[0].plot(t31[0,:],t31[1,:], label='31 day treated in {}'.format(area))
+    ax[1].set_title('Cancer waiting times 62 day standard')
+    ax[1].plot(r62[0,:],r62[1,:], label='62 day referrals in {}'.format(area))
+    ax[1].plot(r62[0,:],r62[1,:], label='62 day referrals in {}'.format(area))
+    ax[2].set_title('Operations cancelled')
+    ax[2].plot(op_tot[0,:],op_can[1,:]/op_tot[1,:], label='perc oper cancelled in {}'.format(area))
+    #ax[2].plot(op_tot[0,:],op_cap[1,:]/op_can[1,:], label='perc canc for capacity in {}'.format(area))
+    ax[3].set_title('Diagnostic waiting times over 4 weeks')
+    ax[3].plot(di4[0,:],di4[1,:], label='imaging 4 weeks in {}'.format(area))
+    ax[3].plot(de4[0,:],de4[1,:], label='endoscopy 4 weeks in {}'.format(area))
+    ax[4].set_title('Diagnostic waiting times over 6 weeks')
+    ax[4].plot(di6[0,:],di6[1,:], label='imaging 6 weeks in {}'.format(area))
+    ax[4].plot(de6[0,:],de6[1,:], label='endoscopy 6 weeks in {}'.format(area))
+    ax[5].set_title('Quarterly covid data')
+    #ax[5].plot(cov_pos[0,:],cov_pos[1,:], label='positive covid in {}'.format(area))
+    ax[5].plot(cov_death[0,:],cov_death[1,:], label='covid deaths in {}'.format(area))
+    
+    for i in range(len(ax)):
+        ax[i].legend()
+        ax[i].tick_params('x',labelrotation=45)
+        every_nth = 1
+        for n, label in enumerate(ax[i].xaxis.get_ticklabels()):
+            if n % every_nth != 0:
+                label.set_visible(False)
+plt.savefig('some_datasets_plotted.png')
+plt.show()

d66819e756ab9005f9cd549e10561543279c1ee1
Sofie Verhees <s.b.verhees@sms.ed.ac.uk>
2022-02-24 14:16:30 +0000

    GLM for pop size and one cancertype

--- final_code/GLM/GLM_one_cancertype.R
+++ final_code/GLM/GLM_one_cancertype.R
@@ -6,6 +6,8 @@ data2 <- read.csv("../data/31days.csv", header = TRUE)
 
 data <- as.data.frame (data)
 
+data <- subset(data, CancerType=='Lung')
+
 logLE <- log(data$PopSize)# -> add for population size
 
 family1 = "poisson"
@@ -15,15 +17,11 @@ family1 = "poisson"
 
 data$HB <- as.factor( data$HB )
 
-data$CancerType <- as.factor( data$CancerType )
-
 levels(data$HB) 
-
-levels(data$CancerType) 
 
 #--------- log-link is chosen by default in Poisson distribution. #--------- 
 
-mod.1 <- glm( NumberOfEligibleReferrals62DayStandard ~  offset(logLE) + HB + CancerType + Date + JustPandemic + Pandemic_cat, data = data, family=family1 )
+mod.1 <- glm( NumberOfEligibleReferrals62DayStandard ~  offset(logLE) + HB + Date + JustPandemic + Pandemic_cat, data = data, family=family1 )
 
 summary(mod.1)
 
@@ -46,10 +44,10 @@ data$rates <- data$NumberOfEligibleReferrals62DayStandard / data$PopSize
 
 #--------- Let's try to make a plot showing fitted data #--------- 
 
-data_plot <- subset(subset(data, HB=='S08000024'), CancerType=='Breast')
+data_plot <- subset(data, HB=='S08000024')
 
 plot(data_plot$Date, data_plot$rates,
-     main="Nr of Referrals in NHS Lothian for Lung Cancer",
+     main="Nr of Referrals in NHS Lothian for Breast Cancer",
      ylab="nr of referrals", xlab='time',
      type="l",
      col="blue", xaxt='n') 
@@ -64,25 +62,13 @@ region_coef <- coef(mod.1)[2:14]
 region_coef['HBS08000015'] <- 0
 region_coef <- region_coef[order(region_coef)]
 
-
-cancer_coef <- coef(mod.1)[15:23]
-cancer_coef['CancerTypeBreast'] <- 0
-cancer_coef <- cancer_coef[order(cancer_coef)]
-
 y_reg <- seq(length(region_coef))
-y_can <- seq(length(cancer_coef))
 
 par(mar=c(6.1,7,4.1,2.1))
 
 plot(region_coef, y_reg,
-  main="Coefficients for regions",
-  pch=19, col='green', yaxt='n',
-  xlab='', ylab='')
-axis(2, at=y_reg, labels=names(region_coef), las=2)
-
-plot(cancer_coef, y_can,
-     main="Coefficients for cancer types",
+     main="Coefficients for regions for Breast cancer",
      pch=19, col='green', yaxt='n',
      xlab='', ylab='')
-axis(2, at=y_can, labels=names(cancer_coef), las=2)
+axis(2, at=y_reg, labels=names(region_coef), las=2)
 
